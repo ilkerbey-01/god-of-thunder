6951d727046338460737633dc23b23e9a6554785
joncloud <jdberube@gmail.com>
2020-04-22 19:19:56 -0700

    Temporary fix for transparency without using masks

--- src/sdl_graphics.cpp
+++ src/sdl_graphics.cpp
@@ -5,13 +5,15 @@
 
 extern SDL_Renderer* ren;
 
-void sdl_graphics_set_palette_color(int16_t color_index) {
+bool sdl_graphics_set_palette_color(int16_t color_index) {
   if (color_index) {
     PALETTE_COLOR color = palette_colors[color_index];
-    SDL_SetRenderDrawColor(ren, color.r, color.g, color.b, 0xff);
+    SDL_SetRenderDrawColor(ren, color.r, color.g, color.b, SDL_ALPHA_OPAQUE);
+    return true;
   }
   else {
-    SDL_SetRenderDrawColor(ren, 0xff, 0xff, 0xff, 0x00);
+    SDL_SetRenderDrawColor(ren, 0xff, 0xff, 0xff, SDL_ALPHA_TRANSPARENT);
+    return false;
   }
 }
 
@@ -24,19 +26,21 @@ void xfillrectangle(int16_t StartX, int16_t StartY, int16_t EndX, int16_t EndY,
   rect.w = EndX - StartX;
   rect.h = EndY - StartY;
 
-  sdl_graphics_set_palette_color(Color);
-  SDL_RenderFillRect(ren, &rect);
-  SDL_RenderPresent(ren);
+  if (sdl_graphics_set_palette_color(Color)) {
+    SDL_RenderFillRect(ren, &rect);
+    SDL_RenderPresent(ren);
+  }
 }
 
 
 void sdl_graphics_render_image(int16_t x, int16_t y, int16_t width, int16_t height, uint8_t* buff) {
   int size = width * height;
   for (int i = 0; i < size; i++) {
-    sdl_graphics_set_palette_color(*(buff + i));
-    int offset_x = x + (i % width);
-    int offset_y = y + (i / width);
-    SDL_RenderDrawPoint(ren, offset_x, offset_y);
+    if (sdl_graphics_set_palette_color(*(buff + i))) {
+      int offset_x = x + (i % width);
+      int offset_y = y + (i / width);
+      SDL_RenderDrawPoint(ren, offset_x, offset_y);
+    }
   }
   SDL_RenderPresent(ren);
 }
@@ -63,10 +67,11 @@ void xtext(int16_t x, int16_t y, uint16_t pagebase, uint8_t* buff, int16_t color
     int16_t text_color = *(buff + i) == 0
       ? 0
       : color;
-    sdl_graphics_set_palette_color(text_color);
-    int offset_x = x + (i % 8);
-    int offset_y = y + (i / 8);
-    SDL_RenderDrawPoint(ren, offset_x, offset_y);
+    if (sdl_graphics_set_palette_color(text_color)) {
+      int offset_x = x + (i % 8);
+      int offset_y = y + (i / 8);
+      SDL_RenderDrawPoint(ren, offset_x, offset_y);
+    }
   }
   SDL_RenderPresent(ren);
 }
