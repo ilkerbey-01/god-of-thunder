c186c50cb6fff51893f171eb245bcbc95ba533ef
joncloud <jdberube@gmail.com>
2020-04-24 16:01:29 -0700

    Removes unnecessary function

--- src/utility.h
+++ src/utility.h
@@ -9,4 +9,3 @@ void delay(int milliseconds);
 int8_t parse_decimal_int8_t(char* str);
 int16_t parse_decimal_int16_t(char* str);
 int32_t parse_decimal_int32_t(char* str);
-void* movedata(void* src0, void* src1, void* dest0, void* dest1, size_t bytes);
